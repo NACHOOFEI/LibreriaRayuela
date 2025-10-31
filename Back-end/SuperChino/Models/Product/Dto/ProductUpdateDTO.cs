92a55eb84e83c76a4b68a5aea5bb2fa0649c519a
lorenzodriva <ldriva2004@gmail.com>
2025-10-30 22:20:16 -0300

    Agrego entidad CUstomer (Model, Dto, Mapping, Repository, Services, Controller)

--- Back-end/SuperChino/Models/Product/Dto/ProductUpdateDTO.cs
+++ Back-end/SuperChino/Models/Product/Dto/ProductUpdateDTO.cs
@@ -5,18 +5,18 @@ namespace SuperChino.Models.Product.Dto
     public class ProductUpdateDTO
     {
         [MaxLength(30, ErrorMessage = "No más de 30 caracteres")]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         [MaxLength(100, ErrorMessage = "No más de 100 caracteres")]
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
         [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser mayor a 0")]
-        public decimal Price { get; set; }
+        public decimal? Price { get; set; }
 
         [Range(0, int.MaxValue, ErrorMessage = "El stock no puede ser negativo")]
-        public int Stock { get; set; }
+        public int? Stock { get; set; }
 
         public string? ImageUrl { get; set; }
-        public int CategoryId { get; set; }
+        public int? CategoryId { get; set; }
     }
 }
---
+++ Back-end/SuperChino/Repositories/CustomerRepository.cs
@@ -0,0 +1,15 @@
+﻿using SuperChino.Config;
+using SuperChino.Models.Customer;
+
+namespace SuperChino.Repositories
+{
+    public interface ICustomerRepository : IRepository<Customer> { }
+    public class CustomerRepository : Repository<Customer>, ICustomerRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CustomerRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
