bd7197f50b5731274011d4b740356bd6856036cd
lorenzodriva <ldriva2004@gmail.com>
2025-10-31 00:37:32 -0300

    Agrego entidad OrderItem (Model, Dto, Mapping, Repository, Services, Controller)

---
+++ Back-end/SuperChino/Models/OrderItem/Dto/OrderItemDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace SuperChino.Models.OrderItem.Dto
+{
+    public class OrderItemDTO
+    {
+        public int Id { get; set; }
+        public Product.Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
