f3314fd508ff7c2949d47f599eff37f16c05e39f
lorenzodriva <ldriva2004@gmail.com>
2025-11-12 16:10:51 -0300

    correccion en la cantidad de caracteres de descripcion

--- Back-end/SuperChino/Models/Product/Dto/ProductUpdateDTO.cs
+++ Back-end/SuperChino/Models/Product/Dto/ProductUpdateDTO.cs
@@ -7,7 +7,7 @@ namespace SuperChino.Models.Product.Dto
         [MaxLength(30, ErrorMessage = "No más de 30 caracteres")]
         public string? Name { get; set; }
 
-        [MaxLength(100, ErrorMessage = "No más de 100 caracteres")]
+        [MaxLength(2000, ErrorMessage = "No más de 2000 caracteres")]
         public string? Description { get; set; }
 
         [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser mayor a 0")]
